6d7410d3ddb98bd23a1e7bd4c3ab1886d11e72fa
gunacodingeverest <gunavardhanytt@gmail.com>
2021-07-27 16:10:42 +0530

    changed last

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://guna:sonu2020india@cluster0.d5osz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
